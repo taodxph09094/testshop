4efa7754e410a1eb48da4cfeac4ec63b7d051366
taodxph09094 <taodxph09094@gmail.com>
2021-12-12 03:00:52 +0700

    api 1

--- api/app.js
+++ api/app.js
@@ -1,15 +1,22 @@
 const express = require("express");
-
 const app = express();
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 
 const errorMiddleware = require("./middleware/error");
 
 app.use(express.json())
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //Route imports
 const product = require("./routers/productRoute");
+const user = require("./routers/userRoute");
+const order = require("./routers/orderRoute");
 
 app.use("/api/v1", product);
+app.use("/api/v1", user);
+app.use("/api/v1", order);
 
 // middleware for errors
 app.use(errorMiddleware);
--- api/utils/apifeatures.js
+++ api/utils/apifeatures.js
@@ -25,16 +25,25 @@ class ApiFeatures {
         const removeFields = ["keyword", "page", "limit"];
 
         removeFields.forEach((key) => delete queryCopy[key]);
-
         // Filter For Price and Rating
-
+        console.log(queryCopy)
         let queryStr = JSON.stringify(queryCopy);
+
         queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (key) => `$${key}`);
 
         this.query = this.query.find(JSON.parse(queryStr));
+        console.log(queryStr);
+        return this;
+
+      }
+      pagination(resultPerPage) {
+        const currentPage = Number(this.queryStr.page) || 1;
+
+        const skip = resultPerPage * (currentPage - 1);
+
+        this.query = this.query.limit(resultPerPage).skip(skip);
 
         return this;
       }
-
 }
 module.exports = ApiFeatures;
